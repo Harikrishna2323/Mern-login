22fe6317cc0bcf7965402a3da686abb8cd85a16f
harikrishna2323 <harikb23@gmail.com>
2021-11-30 11:08:43 +0530

    changed mail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,13 +3,13 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (options) => {
   //create a transporter
   const transporter = nodemailer.createTransport({
-    // service: "Gmail",
+    service: "Gmail",
 
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    // host: process.env.SMTP_HOST,
+    // port: process.env.SMTP_PORT,
     auth: {
-      user: "697262acfe5680",
-      pass: "112794c2ad3185",
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
     },
   });
   // console.log(EMAIL_USERNAME, EMAIL_PASSWORD);
