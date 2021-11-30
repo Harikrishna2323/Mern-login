2fe1beeb8cbb598e63868738427c2e1eec58ea57
harikrishna2323 <harikb23@gmail.com>
2021-11-30 12:23:27 +0530

    added port

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,10 +3,10 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (options) => {
   //create a transporter
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    // service: "Gmail",
 
-    // host: process.env.SMTP_HOST,
-    // port: process.env.SMTP_PORT,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
     auth: {
       user: "697262acfe5680",
       pass: "112794c2ad3185",
